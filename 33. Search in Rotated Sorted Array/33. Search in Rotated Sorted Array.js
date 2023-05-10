254d40cf1e9fd73da4027939ace8e9d89c628fd6
Ian Hwang <meen813@hotmail.com>
2023-05-09 19:21:44 -0700

    binary search

--- 33. Search in Rotated Sorted Array/33. Search in Rotated Sorted Array.js
+++ 33. Search in Rotated Sorted Array/33. Search in Rotated Sorted Array.js
@@ -23,33 +23,32 @@
 // 2~4 과정을 반복합니다.
 
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
-var search = function (nums, target) {
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    var search = function (nums, target) {
 
-    let left = 0, right = nums.length - 1;
+        let left = 0, right = nums.length - 1;
 
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        if (nums[mid] === target) {
-            return mid;
-        }
-        if (nums[mid] >= nums[left]) { // 중간 값이 왼쪽 부분에 속하는 경우
-            if (target >= nums[left] && target < nums[mid]) { // 찾는 값이 중간 값의 왼쪽 부분에 위치하는 경우
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+            if (nums[mid] === target) {
+                return mid;
             }
-        } else { // 중간 값이 오른쪽 부분에 속하는 경우
-            if (target <= nums[right] && target > nums[mid]) { // 찾는 값이 중간 값의 오른쪽 부분에 위치하는 경우
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+            if (nums[mid] >= nums[left]) { // 중간 값이 왼쪽 부분에 속하는 경우
+                if (target >= nums[left] && target < nums[mid]) { // 찾는 값이 중간 값의 왼쪽 부분에 위치하는 경우
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else { // 중간 값이 오른쪽 부분에 속하는 경우
+                if (target <= nums[right] && target > nums[mid]) { // 찾는 값이 중간 값의 오른쪽 부분에 위치하는 경우
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
-    }
-
-    return -1;
-};
+        return -1;
+    };
